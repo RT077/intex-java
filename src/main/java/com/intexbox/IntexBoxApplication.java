1c003551c5cc2c7527750e317de6ba15bbba6fef
RT077 <rupeshtayde444@gmail.com>
2025-10-25 17:28:01 +0530

    3rd commit

--- src/main/java/com/intexbox/IntexBoxApplication.java
+++ src/main/java/com/intexbox/IntexBoxApplication.java
@@ -2,9 +2,10 @@ package com.intexbox;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.SpringServletContainerInitializer;
 
 @SpringBootApplication
-public class IntexBoxApplication {
+public class IntexBoxApplication extends SpringServletContainerInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(IntexBoxApplication.class, args);
